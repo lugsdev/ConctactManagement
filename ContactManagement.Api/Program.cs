80530d1339414491782aa0fc241f63cc5395cb8f
marlamoury <marlamoury@hotmail.com>
2024-10-29 13:39:16 -0300

    Inclusão de Autenticação e Autorização ao projeto

--- ContactManagement.Api/Program.cs
+++ ContactManagement.Api/Program.cs
@@ -1,32 +1,84 @@
 using System.Data;
 using System.Data.SqlClient;
+using System.Text;
 using ContactManagement.Application.Interfaces;
 using ContactManagement.Application.Services;
 using ContactManagement.Application.Validators;
 using ContactManagement.Domain.Interfaces;
 using ContactManagement.InfraStructure.Respositories;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers()
     .AddFluentValidation(config =>
     {
         config.RegisterValidatorsFromAssemblyContaining<ContactDtoValidator>();
     });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+// Configure Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Desafio Fase 1 Grupo2", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization Header - utilize Bearer + seu token",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 string connectionString = builder.Configuration.GetConnectionString("ConnectionMarla");
-builder.Services.AddTransient<IDbConnection>( db => new SqlConnection(connectionString));
+builder.Services.AddTransient<IDbConnection>(db => new SqlConnection(connectionString));
 
 builder.Services.AddScoped<IContactServices, ContactServices>();
 builder.Services.AddScoped<IContactRepository, ContactRepository>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+var key = Encoding.ASCII.GetBytes(builder.Configuration.GetValue<string>("SecretJWT"));
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 var app = builder.Build();
 
@@ -39,6 +91,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication(); // Não esqueça de adicionar o middleware de autenticação
 app.UseAuthorization();
 
 app.MapControllers();
--- ContactManagement.Application/Services/TokenServices.cs
+++ ContactManagement.Application/Services/TokenServices.cs
@@ -1,43 +1,53 @@
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
-using ContactManagement.Application.Configurations;
+using System.Threading.Tasks;
+using ContactManagement.Application.Interfaces;
+using ContactManagement.Domain.Interfaces;
 using ContactManagement.Domain.Models;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
-namespace ContactManagement.Application.Services;
-
-public class TokenServices
+namespace ContactManagement.Application.Services
 {
-    private readonly ConfigurationTokenSettings _settings;
-
-    public TokenServices(IOptions<ConfigurationTokenSettings> settings)
+    public class TokenService : ITokenService
     {
-        _settings = settings.Value;
-    }
-    public string GenerateToken(User user)
-    {
-        //Create an instance for JwtSecurityTokenHandler
-        var handler = new JwtSecurityTokenHandler();
-        
-       //Get the privateKey from appSettings and enconding it
-        var key = Encoding.ASCII.GetBytes(_settings.Key);
+        private readonly IConfiguration _configuration;
+        private readonly IUserRepository _userRepository;
 
-    
-        // Create the credentiasl
-        var credentials = new SigningCredentials(
-            new SymmetricSecurityKey(key),
-            SecurityAlgorithms.HmacSha256Signature);
-
-        var tokenDescriptor = new SecurityTokenDescriptor
+        public TokenService(IConfiguration configuration, IUserRepository userRepository)
         {
-            SigningCredentials = credentials,
-            Expires = DateTime.UtcNow.AddHours(1),
-        };
-        
-        var token = handler.CreateToken(tokenDescriptor);
-        
-        return handler.WriteToken(token);
-        
+            _configuration = configuration;
+            _userRepository = userRepository;
+        }
+
+        public async Task<string> GetToken(User user)
+        {
+            // Validamos se o usuário existe no banco de dados
+            var usuarioDb = await _userRepository.GetByUsernameAsync(user.Username);
+
+            // Verifica se o usuário foi encontrado e se a senha está correta
+            if (usuarioDb == null || usuarioDb.Password != user.Password)
+                return string.Empty;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var chaveCriptografia = Encoding.ASCII.GetBytes(_configuration.GetValue<string>("SecretJWT"));
+
+            var tokenPropriedades = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, usuarioDb.Username),
+                    new Claim(ClaimTypes.Role, usuarioDb.SystemPermission.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(chaveCriptografia),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenPropriedades);
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
